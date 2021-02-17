cfc5b365d1bb170af6cf5e46725141a4813f9301
PreetiGupta1711 <preeti.gupta39@gmail.com>
2021-02-17 22:01:15 +0530

    community comp initial code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './common/Header';
 import Landing from './components/Landing';
+import Community from './components/Community';
 import Location from './components/Location';
 import Footer from './common/Footer';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="main">
       <Header />
       <Landing />
+      <Community />
       <Location />
       <Footer />
     </div>
---
+++ src/components/Community/Community.jsx
@@ -0,0 +1,12 @@
+const Community = () => {
+  return (
+    <section className="community-section">
+      <div className="community-container">
+        <div className="community-title-container">aaaaa</div>
+        <div className="community-title-container"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Community;
